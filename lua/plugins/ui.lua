6aba4d7d7cdd49236a7ebd46995f03def8efa77f
Aushzlten12 <josepachas1604@gmail.com>
2024-01-10 23:19:51 -0500

    Update lazyvim

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,107 @@
+return {
+  {
+    "folke/noice.nvim",
+    opts = function(_, opts)
+      table.insert(opts.routes, {
+        filter = {
+          event = "notify",
+          find = "No information available",
+        },
+        opts = {
+          skip = true,
+        },
+      })
+      opts.presets.lsp_doc_border = true
+    end,
+  },
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      timeout = 10000,
+    },
+  },
+  -- bufferline
+  {
+    "akinsho/bufferline.nvim",
+    keys = {
+      { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
+      { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
+    },
+    opts = {
+      options = {
+        mode = "tabs",
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+      },
+    },
+  },
+  -- statusline
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = {
+      options = {
+        theme = "tokyonight",
+      },
+    },
+  },
+  -- filename
+  {
+    "b0o/incline.nvim",
+    dependencies = { "folke/tokyonight.nvim" },
+    event = "BufReadPre",
+    priority = 1200,
+    config = function()
+      local colors = require("tokyonight.colors").setup()
+      require("incline").setup({
+        highlight = {
+          groups = {
+            InclineNormal = { guibg = "#3C0332", guifg = "#CCCFD9" },
+            InclineNormalNC = { guifg = "#CBD6DD", guibg = "#041A63" },
+          },
+        },
+        window = { margin = { vertical = 0, horizontal = 1 } },
+        hide = {
+          cursorline = true,
+        },
+        render = function(props)
+          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+          if vim.bo[props.buf].modified then
+            filename = "[+]" .. filename
+          end
+
+          local icon, color = require("nvim-web-devicons").get_icon_color(filename)
+
+          return { { icon, guifg = color }, { "" }, { filename } }
+        end,
+      })
+    end,
+  },
+  -- animations
+  {
+    "echasnovski/mini.animate",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      opts.scroll = {
+        enable = false,
+      }
+    end,
+  },
+  -- logo
+  {
+    "nvimdev/dashboard-nvim",
+    event = "VimEnter",
+    opts = function(_, opts)
+      local logo = [[
+ █████╗ ██╗   ██╗███████╗██╗  ██╗ █████╗ ██╗  ████████╗███████╗███╗   ██╗
+██╔══██╗██║   ██║██╔════╝██║  ██║██╔══██╗██║  ╚══██╔══╝██╔════╝████╗  ██║
+███████║██║   ██║███████╗███████║███████║██║     ██║   █████╗  ██╔██╗ ██║
+██╔══██║██║   ██║╚════██║██╔══██║██╔══██║██║     ██║   ██╔══╝  ██║╚██╗██║
+██║  ██║╚██████╔╝███████║██║  ██║██║  ██║███████╗██║   ███████╗██║ ╚████║
+╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝   ╚══════╝╚═╝  ╚═══╝
+      ]]
+      logo = string.rep("\n", 8) .. logo .. "\n\n"
+      opts.config.header = vim.split(logo, "\n")
+    end,
+  },
+}
